60903303dbb383882f565fef75e566677bb40073
Chris <chris@usablehq.com>
2017-10-04 19:42:54 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import * as THREE from 'three';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Pixelheart
+      </div>
+    );
+  }
+}
+
+export default App;
